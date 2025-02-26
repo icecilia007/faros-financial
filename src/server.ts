fcf9378a466c011d22388a2d3c87f680b5258b9d
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2025-02-25 21:35:03 -0300

    fix(server): correct error middleware to handle exceptions properly

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import stockRoutes from "./routes/stock.routes";
 import homeRoutes from "./routes/home.routes";
-import {errorHandler} from "./middlewares/errorHandler.js";
 import { config } from './config/config';
+import {errorHandler} from "./middlewares/error";
 
 console.log("PORT definida no .env:", config.PORT);
 console.log("TOKEN definida no .env:", config.BRAPI_TOKEN);
@@ -16,8 +16,11 @@ app.use(express.json());
 
 app.use("/api/stocks", stockRoutes);
 app.use("/api/home", homeRoutes);
-// TODO
-//app.use(errorHandler);
+
+
+app.use((err: unknown, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    errorHandler(err, req, res, next);
+});
 app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
